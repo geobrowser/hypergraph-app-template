e0aa9a98a8775985a1f5d0ac29ffae584da794dc
Nik Graf <nik@nikgraf.com>
2025-07-04 16:41:10 +0200

    update gallery

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,37 +1,19 @@
-import { Card } from './card';
 import { Project } from '@/schema';
-import {
-  useQuery,
-} from '@graphprotocol/hypergraph-react';
+import { useQuery } from '@graphprotocol/hypergraph-react';
+import { Card } from './card';
 
 export function Gallery() {
-
   const { data: projects } = useQuery(Project, {
     mode: 'public',
-    include: { avatar: {}}})
+    include: { avatar: {} },
+  });
 
-    
-    console.log(projects)
-
-  const items = [
-    {
-      name: 'Project Alpha',
-      coverImage: 'https://picsum.photos/400/300?random=1'
-    },
-    {
-      name: 'Project Beta',
-      coverImage: 'https://picsum.photos/400/300?random=2'
-    }
-  ];
+  const projectsWithAvatar = projects?.filter((project) => project.avatar.length > 0);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-6">
-      {projects.slice(0,12).map((project, index) => (
-        <Card 
-          key={index}
-          name={project.name}
-          image={project.cover.url}
-        />
+      {projectsWithAvatar.slice(0, 12).map((project, index) => (
+        <Card key={index} name={project.name} image={project.avatar[0].url} />
       ))}
     </div>
   );
