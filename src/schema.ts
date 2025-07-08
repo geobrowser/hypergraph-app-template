68c500b1e6e93cd4a6a0e9a203212edca519e7c5
Nik Graf <nik@nikgraf.com>
2025-07-08 14:12:22 +0200

    format schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,5 +1,5 @@
 import { Entity, Type } from '@graphprotocol/hypergraph';
 export class Address extends Entity.Class<Address>('Address')({
-name: Type.Text,
-description: Type.Text,
+  name: Type.Text,
+  description: Type.Text,
 }) {}
